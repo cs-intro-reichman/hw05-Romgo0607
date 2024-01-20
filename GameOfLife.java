7b85405d8c27571d7919b6b567b3b20d26445b55
Romi Golan <romi.golan@post.runi.ac.il>
2024-01-20 12:44:59 +0200

    test

--- GameOfLife.java
+++ GameOfLife.java
@@ -154,9 +154,9 @@ public class GameOfLife {
     public static void print(int[][] arr) {
             for (int i = 0; i < arr.length; i++) {
 				for (int j = 0; j < arr[i].length; j++){
-					System.out.printf("%3d", arr[i][j]);
+					System.out.printf("%d", arr[i][j]);
 				}
-				System.out.printf("%n%n");
+				System.out.printf("%n");
 			}
 	}
 		
